593eb5e499569d77a31485c65cb2595a7952f141
Naoki504 <c0a20015b9@edu.teu.ac.jp>
2023-06-05 23:23:10 +0900

    プログラムを修正しました。

--- Intern-Practice/第6号/トレーニング3/ストップウォッチ2/ストップウォッチ2/Form_Main.cs
+++ Intern-Practice/第6号/トレーニング3/ストップウォッチ2/ストップウォッチ2/Form_Main.cs
@@ -12,40 +12,41 @@ namespace ストップウォッチ2
     
         private void button_Start_Click(object sender, EventArgs e)
         {
-            this.timer.Enabled= true;
+            timer.Enabled= true;
 
-            this.button_Stop.Enabled= true;
-            this.button_Lap.Enabled= true;
-            this.button_Reset.Enabled = false;
-            this.button_Start.Enabled = false;
+            button_Stop.Enabled= true;
+            button_Lap.Enabled= true;
+            button_Reset.Enabled = false;
+            button_Start.Enabled = false;
         }
 
         private void button_Stop_Click(object sender, EventArgs e)
         {
-            this.timer.Enabled = false;
+            timer.Enabled = false;
 
-            this.button_Start.Enabled = true;
-            this.button_Reset.Enabled = true;
-            this.button_Stop.Enabled = false;
-            this.button_Lap.Enabled = false;
+            button_Start.Enabled = true;
+            button_Reset.Enabled = true;
+            button_Stop.Enabled = false;
+            button_Lap.Enabled = false;
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            this.countTimer = this.countTimer.AddSeconds(1.0);
-            this.label_display.Text = this.countTimer.ToString("mm:ss");
+            countTimer = countTimer.AddSeconds(1.0);
+            label_display.Text = countTimer.ToString("mm:ss");
         }
 
         private void button_Reset_Click(object sender, EventArgs e)
         {
-            this.countTimer = DateTime.Parse("00:00:00");
-            this.label_display.Text = this.countTimer.ToString("mm:ss");
+            countTimer = DateTime.Parse("00:00:00");
+            label_display.Text = countTimer.ToString("mm:ss");
+            listBox_LapTime.Items.Clear();
         }
 
         private void button_Lap_Click(object sender, EventArgs e)
         {
             
-            this.listBox_LapTime.Items.Add(label_display.Text);
+            listBox_LapTime.Items.Add(label_display.Text);
         }
     }
 }
