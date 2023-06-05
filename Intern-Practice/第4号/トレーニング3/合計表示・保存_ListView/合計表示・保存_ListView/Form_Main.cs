72ba00ce9049406eed7c242208d2b1b088ec35b4
Naoki504 <c0a20015b9@edu.teu.ac.jp>
2023-06-05 23:51:14 +0900

    プログラムを修正しました。

--- Intern-Practice/第4号/トレーニング3/合計表示・保存_ListView/合計表示・保存_ListView/Form_Main.cs
+++ Intern-Practice/第4号/トレーニング3/合計表示・保存_ListView/合計表示・保存_ListView/Form_Main.cs
@@ -9,32 +9,32 @@ namespace 合計表示_保存_ListView
         private List<string[]> products= new List<string[]>();
         private void button_Display_Click(object sender, EventArgs e)
         {
-            this.openFileDialog.ShowDialog();
+            openFileDialog.ShowDialog();
         }
         private void openFileDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            readData(this.openFileDialog.FileName);
+            readData(openFileDialog.FileName);
             displayData();
         }
 
         private void readData(string fileName) 
         { 
-            this.products.Clear();
+            products.Clear();
             StreamReader sr = new StreamReader(fileName);
 
             while(-1 < sr.Peek())
             {
                 string[] product = sr.ReadLine().Split(" ");
-                this.products.Add(product);
+                products.Add(product);
             }
             sr.Close();
         }
         private void displayData() 
         { 
-            this.listView_display.Items.Clear();
-            for(int i = 0; i < this.products.Count; i++)
+            listView_display.Items.Clear();
+            for(int i = 0; i < products.Count; i++)
             {
-                string[] product = this.products[i];
+                string[] product = products[i];
 
                 int total = int.Parse(product[1]) * int.Parse(product[2]);
 
@@ -42,7 +42,7 @@ namespace 合計表示_保存_ListView
                                 ,total.ToString()};
                 ListViewItem item = new ListViewItem(displayProduct);
 
-                this.listView_display.Items.Add(item);
+                listView_display.Items.Add(item);
 
             
             }
@@ -50,32 +50,32 @@ namespace 合計表示_保存_ListView
 
         private void button_delete_Click(object sender, EventArgs e)
         {
-            if(this.listView_display.SelectedIndices.Count == 0)
+            if(listView_display.SelectedIndices.Count == 0)
             {
                 MessageBox.Show("削除する項目を選択してください。");
                 return;
             }
-            int index = this.listView_display.SelectedIndices[0];
-            this.products.RemoveAt(index);
+            int index = listView_display.SelectedIndices[0];
+            products.RemoveAt(index);
 
             displayData();
         }
 
         private void button_save_Click(object sender, EventArgs e)
         {
-            this.saveFileDialog.ShowDialog();
+            saveFileDialog.ShowDialog();
         }
 
         private void saveFileDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            saveData(this.saveFileDialog.FileName+".txt");
+            saveData(saveFileDialog.FileName);
         }
         private void saveData(string fileNema)
         {
             StreamWriter sw = new StreamWriter(fileNema);
-            for (int i = 0; i < this.products.Count; i++)
+            for (int i = 0; i < products.Count; i++)
             {
-                string[] product = this.products[i];
+                string[] product = products[i];
                 sw.WriteLine(product[0]+" "+product[1]+" "+ product[2]);
             }
 
