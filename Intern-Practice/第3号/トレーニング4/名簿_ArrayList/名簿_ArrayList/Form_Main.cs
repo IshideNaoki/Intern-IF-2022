5e15ffe31488f9858c1f84ca580938e1a4cb0a8b
Naoki504 <c0a20015b9@edu.teu.ac.jp>
2023-06-05 23:01:55 +0900

    プログラムを修正しました。

--- Intern-Practice/第3号/トレーニング4/名簿_ArrayList/名簿_ArrayList/Form_Main.cs
+++ Intern-Practice/第3号/トレーニング4/名簿_ArrayList/名簿_ArrayList/Form_Main.cs
@@ -13,14 +13,14 @@ namespace 名簿_ArrayList
             
         private void button_Set_Click(object sender, EventArgs e)
         {
-            setData(this.textBox_FamilyName.Text, this.textBox_Name.Text);
+            setData(textBox_FamilyName.Text, textBox_Name.Text);
 
             showData();
         }
         private void setData(string familyName, string name)
         {
             string[] personalData = new string[2];
-            if (this.radioButton_Man.Checked)
+            if (radioButton_Man.Checked)
             {
                 personalData[0] = "男性";
             }
@@ -30,28 +30,28 @@ namespace 名簿_ArrayList
             }
 
             personalData[1] = familyName + " " + name;
-            this.members.Add(personalData);
+            members.Add(personalData);
         }
         private void showData()
         {
-            this.listBox_Display.Items.Clear();
-            for (int i = 0; i < this.members.Count; i++)
+            listBox_Display.Items.Clear();
+            for (int i = 0; i < members.Count; i++)
             {
-                string[] data = (string[])this.members[i];
+                string[] data = (string[])members[i];
 
                 string displayString = data[0] + "\t" + data[1];
-                this.listBox_Display.Items.Add(displayString);
+                listBox_Display.Items.Add(displayString);
             }
         }
         private void button_delete_Click(object sender, EventArgs e)
         {
-            if (this.listBox_Display.SelectedIndices.Count == 0)
+            if (listBox_Display.SelectedIndices.Count == 0)
             {
                 MessageBox.Show("削除する項目を選択してください。");
             }
 
-            int index = this.listBox_Display.SelectedIndices[0];
-            this.members.RemoveAt(index);
+            int index = listBox_Display.SelectedIndices[0];
+            members.RemoveAt(index);
             showData();
         }
     }
