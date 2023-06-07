f6d4c3f52964f737d09160284968229f9fed9196
Naoki504 <c0a20015b9@edu.teu.ac.jp>
2023-06-07 23:05:11 +0900

    プログラムを修正しました。

--- Intern-Practice/第2号/練習2/合計を戻り値で返す/合計を戻り値で返す/Program.cs
+++ Intern-Practice/第2号/練習2/合計を戻り値で返す/合計を戻り値で返す/Program.cs
@@ -11,18 +11,20 @@ namespace 合計を戻り値で返す
         public static void Main(string[] args)
         {
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            total_sum(numbers);
+            int sum = total_sum(numbers);
+            Console.WriteLine("配列の要素の合計:" + sum);
 
         }
 
-        private static void total_sum(int[] array_num)
+        private static int total_sum(int[] array_num)
         {
             int sum = 0;
             for (int i = 0; i < array_num.Length; i++)
             {
                 sum += array_num[i];
             }
-            Console.WriteLine("配列の要素の合計:" + sum);
+            return sum;
+            
         }
 
     }
