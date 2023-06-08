0cdf39c9850d4bce0114f41dc0aef5d92524a334
Naoki504 <c0a20015b9@edu.teu.ac.jp>
2023-06-09 01:01:49 +0900

    プログラムを追加しました。

--- sort_list/sort_list/Program.cs
+++ sort_list/sort_list/Program.cs
@@ -12,19 +12,19 @@ namespace sort_list
             List<int> array = new List<int>();//Listの宣言
 
             string count_num;//ListのSize
-            
-            while(true)//要素数が数字かどうか
+
+            while (true)//要素数が数字かどうか
             {
                 Console.Write("ListのSize(自然数)を入力してください =>");
                 count_num = Console.ReadLine();
 
                 int check_num;
 
-                if(count_num == "0")//0を入力した場合
+                if (count_num == "0")//0を入力した場合
                 {
                     Console.WriteLine("0を入力しないでください");
                 }
-                else if(int.TryParse(count_num, out check_num))//0以外の数を入力した場合
+                else if (int.TryParse(count_num, out check_num))//0以外の数を入力した場合
                 {
                     break;
                 }
@@ -37,15 +37,15 @@ namespace sort_list
             Console.WriteLine();
 
             //Listに要素を追加する
-            for(int i = 0; i < int.Parse(count_num); i++) 
+            for (int i = 0; i < int.Parse(count_num); i++)
             {
                 Console.Write("Listに追加する数字を入力してください =>");
                 string input_num = Console.ReadLine();
                 array.Add(int.Parse(input_num));
             }
-            
+
             Console.WriteLine();
-            
+
             List<int> array_sort = new List<int>();
 
             //昇順or降順に並べ替える
@@ -75,23 +75,23 @@ namespace sort_list
                 Console.WriteLine();
             }
             //Sortした配列の要素を出力
-            for(int i = 0; i <array_sort.Count;i++) 
+            for (int i = 0; i < array_sort.Count; i++)
             {
-                Console.Write(array_sort[i]+" ");
+                Console.Write(array_sort[i] + " ");
             }
 
             Console.ReadLine();
-            
+
         }
         //昇順sortメソッド
-        
+
         private static List<int> asc_sort(List<int> num_array)
         {
-            for(int i = 0; i <num_array.Count; i++) 
-            { 
-                for(int j = num_array.Count-1; j > i;j--)
+            for (int i = 0; i < num_array.Count; i++)
+            {
+                for (int j = num_array.Count - 1; j > i; j--)
                 {
-                    if (num_array[j] < num_array[j-1])
+                    if (num_array[j] < num_array[j - 1])
                     {
                         int tmp = num_array[j - 1];
                         num_array[j - 1] = num_array[j];
